9a10a49a9300a61adae31e0cd87383c618b61f5c
BDS <dumitru.boila@gmail.com>
2019-08-03 11:52:12 +0300

    Simplified page rules in order to allow extension to be active for each http and https urls

--- background.js
+++ background.js
@@ -3,19 +3,18 @@
 // found in the LICENSE file.
 
 'use strict';
-
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
-  });
-
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  // Replace the current rules
+  chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {
+            schemes: [ 'http', 'https']
+          }
+        })
       ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+      actions: [
+        new chrome.declarativeContent.ShowPageAction()
+      ]
+  }])
 });
