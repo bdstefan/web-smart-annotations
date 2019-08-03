74be843e31c9513103ab5901d281e3bbb984a5de
BDS <dumitru.boila@gmail.com>
2019-08-03 12:03:17 +0300

    Small refactoring, using arrow functions and same tab query tuple

--- popup.js
+++ popup.js
@@ -4,27 +4,28 @@
 
 'use strict';
 
+let tabQueryInfo = {active: true, currentWindow: true};
+
 let changeColor = document.getElementById('changeColor');
 let revertColor = document.getElementById('revertColor');
 let annotate    = document.getElementById('annotate');
 let currentURL;
 
-chrome.tabs.query(
-    {'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-    function(tabs){
-      //getCurrentURL(tabs[0].url);
-      currentURL = tabs[0].url;
-});
 
-chrome.storage.sync.get('color', function(data) {
+/**
+ * Get active tab URL address
+ */
+chrome.tabs.query(tabQueryInfo, (tabs) => {currentURL = tabs[0].url;});
+
+chrome.storage.sync.get('color', (data) => {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
 });
 
 
-changeColor.onclick = function(element) {
+changeColor.onclick = (element) => {
   let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.query(tabQueryInfo, (tabs) => {
     chrome.tabs.executeScript(
         tabs[0].id,
         {code: 'document.body.style.backgroundColor = "' + color + '";'});
@@ -32,7 +33,7 @@ changeColor.onclick = function(element) {
 };
 
 revertColor.onclick = ()=> {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.query(tabQueryInfo, (tabs) => {
     chrome.tabs.executeScript(
         tabs[0].id,
         {code: 'document.body.style.backgroundColor = "white";'});
